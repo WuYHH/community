f6138233b5b310562a5566242378e6b4784b15aa
WuYHH <1824162728@qq.com>
2023-04-26 15:22:12 +0800

    update application

--- src/main/java/com/example/demo/Demo3Application.java
+++ src/main/java/com/example/demo/Demo3Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo3Application {
     public static void main(String[] args) {
-        System.out.println("demo");
+        System.out.println("demo1111");
         SpringApplication.run(Demo3Application.class, args);
     }
 
